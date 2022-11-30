5e7a9e82bccc02d7406dd5a6eb8e08f64541e2d4
ciccinne <nordence@student.chalmers.se>
2022-11-30 13:30:35 +0100

    scania platform och tester

---
+++ test/testScania.java
@@ -0,0 +1,22 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.awt.*;
+import org.junit.jupiter.api.Test;
+
+import src.Truck;
+import src.Scania;
+
+public class testScania {
+    @Test
+    public void does_gas_not_gas_when_platform_is_up(){
+        Scania scania = new Scania(2, Color.red, 125, "Saab95", 50, 50);
+        scania.platformUp(30);
+        scania.gas(0.5);
+        assertEquals(0,scania.getCurrentSpeed());
+    }
+   
+
+}
+
