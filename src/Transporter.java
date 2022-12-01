d0185ac68617dac118c7331761b66f36cbac7269
ciccinne <nordence@student.chalmers.se>
2022-12-01 13:26:30 +0100

    hej

--- src/Transporter.java
+++ src/Transporter.java
@@ -3,10 +3,27 @@ import java.awt.*;
 
 public class Transporter extends Truck{
     private IPlatform flatbed;
+    int carsLoaded = 0;
 
     public Transporter(int nrDoors, Color color, int enginePower, String modelString, double X, double Y) {
         super(nrDoors, color, enginePower, modelString, X, Y);
         this.flatbed = new Flatbed();                                    // Composition -> vi skapar en instans av platform och lägger den som ett attrebut till instanserna av scania 
+    }
+
+    public void loadCars(){
+        if (state.flatbed() == 0){  //har inte returnat state än?
+            carsLoaded += 1; // vill kolla att carClose sen load?
+        }  
+    }
+
+    public void maxCars(){
+        if (amountOfCars() == någon siffra){
+            //ge felmeddelande eller sluta load
+        }
+    }
+
+    public void carClose(){
+        if (någon typ av avstånd, fattar inte)
     }
 
     @Override
