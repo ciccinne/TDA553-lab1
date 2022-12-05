9cb4781941c3a664ae52ad67f1ee1852838854d4
aaa <agnes.tomsmark@gmail.com>
2022-12-05 12:15:05 +0100

    Försökte fixa turbo-klass, gick inte så bra.

--- src/Saab95.java
+++ src/Saab95.java
@@ -2,24 +2,24 @@
 import java.awt.*;
 
 public class Saab95 extends Car {  
-    private boolean turboOn;                                        // Flyttat från konstruktorn
+    private Turbo turbo = new Turbo();
     
     public Saab95(double X, double Y) {     
         super(2, Color.red, 125, "Saab95", X, Y);              
-	    turboOn = false;
+	    turbo.setTurboOff();
     }
 
-    public void setTurboOn(){
-	    turboOn = true;
+    public void turboOn(){
+        turbo.setTurboOn(); 
     }
 
-    public void setTurboOff(){   
-	    turboOn = false;
-    }
+
+
+
     @Override 
     protected double speedFactor(){  // Ska overridea
         double turbo = 1;
-        if(turboOn) turbo = 1.3;
+        if (turbo.turboOn) turbo = 1.3;
         return getEnginePower() * 0.01 * turbo;                     // getEnginePower() ist för att kalla på attributet enginePower (enginePower är ju private i Car)
     }
 }
---
+++ src/Turbo.java
@@ -0,0 +1,16 @@
+public class Turbo {
+    private boolean turboOn;
+
+    public Turbo() {
+        this.turboOn = false;
+    }
+
+
+    public void setTurboOn(){
+	    turboOn = true;
+    }
+
+    public void setTurboOff(){   
+	    turboOn = false;
+    }
+}
