f0b2bcb7e4e523c4baf134b41039f126d141753b
aaa <agnes.tomsmark@gmail.com>
2022-12-05 10:32:35 +0100

    Försökte fixa ILoadable-grejen med namnet

--- test/testScania.java
+++ test/testScania.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.awt.*;
 import org.junit.jupiter.api.Test;
 
-import src.Truck;
 import src.Scania;
 
 public class testScania {
